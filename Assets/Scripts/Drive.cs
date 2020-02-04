ace7b84243b334cb5300924dd28c1258ba66bf05
og <ogugu@test.com>
2020-02-04 14:33:01 +0900

    [add] Driveクラスを追加

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float pitch = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        pitch *= Time.deltaTime;
+
+        transform.Translate(0,0,translation);
+        transform.Rotate(Vector3.up, rotation);
+        transform.Rotate(Vector3.left, pitch);
+    }
+}
